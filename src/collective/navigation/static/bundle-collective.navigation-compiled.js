eff1225cd0011ec2b6e59fb2c54ad8948ce6b494
Johannes Raggam <thetetet@gmail.com>
2018-08-14 16:54:43 +0200

    compiled

--- src/collective/navigation/static/bundle-collective.navigation-compiled.js
+++ src/collective/navigation/static/bundle-collective.navigation-compiled.js
@@ -12,9 +12,20 @@ define('collective.navigation',[
             var href = window.location.href;
             $('a', this.$el).each(function () {
                 if (href.indexOf(this.href) !== -1) {
-                    $(this).parent().addClass('inPath');
+                    var parent = $(this).parent();
+
+                    // check the input-openers within the path
+                    var check = parent.find('> input');
+                    if (check.length) {
+                        check[0].checked = true;
+                    }
+
+                    // set "inPath" to all nav items which are within the current path
+                    parent.addClass('inPath');
+
+                    // set "current" to the current selected nav item, if it is in the navigation structure.
                     if (href == this.href) {
-                        $(this).parent().addClass('current');
+                        parent.addClass('current');
                     }
                 }
             });
