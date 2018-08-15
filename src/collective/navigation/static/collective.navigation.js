76744cdaf4f9ebda8f23f525d3bc18bada0a53f4
Peter Holzer <peter.holzer@agitator.com>
2018-08-15 15:22:49 +0200

    we need a switch to precheck checkboxes only for small devices

--- src/collective/navigation/static/collective.navigation.js
+++ src/collective/navigation/static/collective.navigation.js
@@ -9,15 +9,18 @@ define([
         parser: 'mockup',
 
         init: function() {
+            var smallDevice = false
             var href = document.querySelector('head link[rel="canonical"]').href || window.location.href;
             $('a', this.$el).each(function () {
                 if (href.indexOf(this.href) !== -1) {
                     var parent = $(this).parent();
 
-                    // check the input-openers within the path
-                    var check = parent.find('> input');
-                    if (check.length) {
-                        check[0].checked = true;
+                    if (smallDevice) {  // only check checkboxes if it's a small device
+                        // check the input-openers within the path
+                        var check = parent.find('> input');
+                        if (check.length) {
+                            check[0].checked = true;
+                        }
                     }
 
                     // set "inPath" to all nav items which are within the current path
@@ -31,7 +34,20 @@ define([
             });
         }
 
+
     });
+
+     function myFunction(smallDevice) {
+        if (smallDevice.matches) { // If media query matches
+            document.body.style.backgroundColor = "yellow";
+        } else {
+            document.body.style.backgroundColor = "pink";
+        }
+    }
+
+    var smallDevice = window.matchMedia("(max-width: 767px)")
+    myFunction(smallDevice) // Call listener function at run time
+    smallDevice.addListener(myFunction) // Attach listener function on state changes
 
     return Navigation;
 });
